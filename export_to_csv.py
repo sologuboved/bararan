1a6101442aeee7c1ddc10100e3b07ebba7210b05
Anna Sakoyan <ansakoy@gmail.com>
2016-05-10 00:31:09 +0300

    yet another upd export

--- export_to_csv.py
+++ export_to_csv.py
@@ -4,6 +4,10 @@ import requests
 import json
 import csv
 
+# По умолчанию - греческий. Для переключения на армянский присвоить ARM значение True
+
+ARM = False
+
 TRANSLATION = 'translation'
 ARTICLE = 'articles'
 WORD = 'word'
@@ -15,7 +19,15 @@ IDIOMS = 'idioms'
 MISC = 'misc'
 JSON_FILE = 'json_lexilogio.json'
 JSON_URL = 'https://raw.githubusercontent.com/sologuboved/bararan/master/json_lexilogio.json'
-# JSON_FILE = 'json_bararan.json'
+LANGUAGE = 'greek'
+OUTPUT_FILE_NAME = 'exported_lexilogio'
+CSV_KEYS = [TRANSLATION, ARTICLE, WORD, CATEGORIES, INSTALLMENT, EXAMPLE, GRAMMAR, MISC]
+if ARM:
+    JSON_FILE = 'json_bararan.json'
+    LANGUAGE = 'armenian'
+    OUTPUT_FILE_NAME = 'exported_bararan'
+    CSV_KEYS = [TRANSLATION, WORD, CATEGORIES, INSTALLMENT, EXAMPLE, GRAMMAR, IDIOMS, MISC]
+
 
 
 def load_json_from_url(json_url):
@@ -30,35 +42,39 @@ def load_json_from_file(json_file):
         return dictionary
 
 
-def extract_values(dictionary):
+def extract_values(dictionary, lang):
     extracted = list()
 
-    # Для греческого
+    if lang == 'greek':
 
-    for entry in dictionary:
-        temp_dict = {TRANSLATION: ', '.join(dictionary[entry][TRANSLATION]).encode('utf-8'),
-                     ARTICLE: ', '.join(dictionary[entry][ARTICLE]).encode('utf-8'),
-                     WORD: dictionary[entry][WORD].encode('utf-8'),
-                     CATEGORIES: ', '.join(dictionary[entry][CATEGORIES]).encode('utf-8'),
-                     INSTALLMENT: dictionary[entry][INSTALLMENT].encode('utf-8'),
-                     EXAMPLE: ', '.join(dictionary[entry][EXAMPLE]).encode('utf-8'),
-                     GRAMMAR: ', '.join(dictionary[entry][GRAMMAR]).encode('utf-8'),
-                     MISC: ', '.join(dictionary[entry][MISC]).encode('utf-8')}
-        extracted.append(temp_dict)
+        # Для греческого
+
+        for entry in dictionary:
+            temp_dict = {TRANSLATION: ', '.join(dictionary[entry][TRANSLATION]).encode('utf-8'),
+                         ARTICLE: ', '.join(dictionary[entry][ARTICLE]).encode('utf-8'),
+                         WORD: dictionary[entry][WORD].encode('utf-8'),
+                         CATEGORIES: ', '.join(dictionary[entry][CATEGORIES]).encode('utf-8'),
+                         INSTALLMENT: dictionary[entry][INSTALLMENT].encode('utf-8'),
+                         EXAMPLE: ', '.join(dictionary[entry][EXAMPLE]).encode('utf-8'),
+                         GRAMMAR: ', '.join(dictionary[entry][GRAMMAR]).encode('utf-8'),
+                         MISC: ', '.join(dictionary[entry][MISC]).encode('utf-8')}
+            extracted.append(temp_dict)
+
+    elif lang == 'armenian':
 
     # Для армянского
 
-    # for entry in dictionary:
-    #     temp_dict = {TRANSLATION: ', '.join(dictionary[entry][TRANSLATION]).encode('utf-8'),
-    #                  WORD: dictionary[entry][WORD].encode('utf-8'),
-    #                  CATEGORIES: ', '.join(dictionary[entry][CATEGORIES]).encode('utf-8'),
-    #                  INSTALLMENT: dictionary[entry][INSTALLMENT].encode('utf-8'),
-    #                  EXAMPLE: ', '.join(dictionary[entry][EXAMPLE]).encode('utf-8'),
-    #                  GRAMMAR: ', '.join(dictionary[entry][GRAMMAR]).encode('utf-8'),
-    #                  IDIOMS: ', '.join(dictionary[entry][IDIOMS]).encode('utf-8'),
-    #                  MISC: ', '.join(dictionary[entry][MISC]).encode('utf-8')}
-    #     extracted.append(temp_dict)
-    # return extracted
+        for entry in dictionary:
+            temp_dict = {TRANSLATION: ', '.join(dictionary[entry][TRANSLATION]).encode('utf-8'),
+                         WORD: dictionary[entry][WORD].encode('utf-8'),
+                         CATEGORIES: ', '.join(dictionary[entry][CATEGORIES]).encode('utf-8'),
+                         INSTALLMENT: dictionary[entry][INSTALLMENT].encode('utf-8'),
+                         EXAMPLE: ', '.join(dictionary[entry][EXAMPLE]).encode('utf-8'),
+                         GRAMMAR: ', '.join(dictionary[entry][GRAMMAR]).encode('utf-8'),
+                         IDIOMS: ', '.join(dictionary[entry][IDIOMS]).encode('utf-8'),
+                         MISC: ', '.join(dictionary[entry][MISC]).encode('utf-8')}
+            extracted.append(temp_dict)
+    return extracted
 
 
 def write_csv(lst_of_dicts, header_list, file_name):
@@ -76,12 +92,7 @@ def write_csv(lst_of_dicts, header_list, file_name):
 
 if __name__ == '__main__':
 
-    # Для греческого
-    csv_keys = [TRANSLATION, ARTICLE, WORD, CATEGORIES, INSTALLMENT, EXAMPLE, GRAMMAR, MISC]
-
-    # Для армянского
-    # csv_keys = [TRANSLATION, WORD, CATEGORIES, INSTALLMENT, EXAMPLE, GRAMMAR, IDIOMS, MISC]
-    # my_dict = load_json_from_file(JSON_FILE)
+    # my_dict = load_json_from_url(JSON_URL)
     my_dict = load_json_from_file(JSON_FILE)
-    values = extract_values(my_dict)
-    write_csv(values, csv_keys, 'exported_lexilogio')
+    values = extract_values(my_dict, LANGUAGE)
+    write_csv(values, CSV_KEYS, OUTPUT_FILE_NAME)
